3b85cee3773abc595dac961cb4a18cf57d521e93
112frosta <112frosta@proton.me>
2024-07-12 23:32:17 +0200

    Optimize imports and update Hud component

--- ui/src/elements/Hud.tsx
+++ ui/src/elements/Hud.tsx
@@ -1,8 +1,7 @@
 import IconCard from "@/components/IconCard";
-import { Fragment, useEffect, useState } from "react";
+import { useState, useCallback } from "react";
 import { useWheel } from "@/hooks/useWheel";
-import { useAnimate, Variants } from "framer-motion";
-
+import { AnimatePresence, useAnimate, Variants } from "framer-motion";
 import {
   FaHeart,
   FaShield,
@@ -10,72 +9,61 @@ import {
   FaDroplet,
   FaMicrophone,
 } from "react-icons/fa6";
+import { cn } from "@/utils/cn";
 
 type ItemName = "Health" | "Armor" | "Hunger" | "Thirst" | "Talk";
 
 const items = [
-  { name: "Health", value: 50, icon: FaHeart },
-  { name: "Armor", value: 0, icon: FaShield },
-  { name: "Hunger", value: 100, icon: FaBurger },
-  { name: "Thirst", value: 69, icon: FaDroplet },
-  { name: "Talk", value: 33, icon: FaMicrophone },
+  { name: "Health", value: 50, Icon: FaHeart },
+  { name: "Armor", value: 0, Icon: FaShield },
+  { name: "Hunger", value: 100, Icon: FaBurger },
+  { name: "Thirst", value: 69, Icon: FaDroplet },
+  { name: "Talk", value: 33, Icon: FaMicrophone },
 ];
 
 const variants: Variants = {
-  hide: {
-    display: "none",
-    opacity: 0,
-    y: 100,
-  },
-  show: {
-    display: "flex",
-    opacity: 1,
-    y: 0,
-  },
+  hide: { display: "none", opacity: 0, y: 100 },
+  show: { display: "flex", opacity: 1, y: 0 },
 };
 
 export default function Hud() {
   const [scope, animate] = useAnimate();
   const [values, setValues] = useState(items);
 
-  useWheel("down", () => {
-    animate(scope.current, variants.hide);
-  });
+  const handleValueChange = useCallback((name: ItemName, value: number) => {
+    setValues((currentValues) =>
+      currentValues.map((item) =>
+        item.name === name ? { ...item, value: item.value + value } : item
+      )
+    );
+  }, []);
 
-  useWheel("up", () => {
-    animate(scope.current, variants.show);
-  });
-
-  useEffect(() => {
-    console.log(values);
-  }, [values]);
-
-  // @ts-ignore: Required function, unsued variable
-  const handleValueChange = (name: ItemName) => {
-    const newValues = values.map((item) => {
-      if (item.name === name) {
-        return { ...item, value: item.value + 20 };
-      }
-      return item;
-    });
-
-    setValues(newValues);
-  };
+  useWheel("down", () => animate(scope.current, variants.hide));
+  useWheel("up", () => animate(scope.current, variants.show));
 
   return (
     <div className="absolute -translate-x-1/2 bottom-5 left-1/2">
-      <div ref={scope} className="flex items-center gap-0.5">
-        {items.map((item, index) => (
-          <Fragment key={index}>
-            {item.name === "Armor" && item.value === 0 ? null : (
+      <button
+        className="bg-red-500"
+        onClick={() => handleValueChange("Armor", -10)}
+      >
+        Debug Button
+      </button>
+      <div ref={scope} className="flex items-center">
+        <AnimatePresence>
+          {values.map(({ name, value, Icon }) =>
+            name === "Armor" && value <= 0 ? null : (
               <IconCard
-                key={item.name}
-                icon={<item.icon className="size-5" />}
-                progress={values[index].value}
+                key={name}
+                icon={<Icon className="size-5" />}
+                progress={value}
+                className={cn(
+                  "mx-0.5 first-of-type:rounded-l-md last-of-type:rounded-r-md"
+                )}
               />
-            )}
-          </Fragment>
-        ))}
+            )
+          )}
+        </AnimatePresence>
       </div>
     </div>
   );
