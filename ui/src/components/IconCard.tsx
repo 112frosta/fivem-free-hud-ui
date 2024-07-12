4cdc53306ae390ee15c320349987c04ac0e108a9
112frosta <112frosta@proton.me>
2024-07-12 23:32:05 +0200

    Update IconCard component with framer-motion animation and props handling

--- ui/src/components/IconCard.tsx
+++ ui/src/components/IconCard.tsx
@@ -1,16 +1,41 @@
-interface IconCardProps {
+import { motion, Variants } from "framer-motion";
+import { cn } from "@/utils/cn";
+
+interface IconCardProps extends React.HTMLAttributes<HTMLDivElement> {
   icon: React.ReactNode;
   progress?: number;
 }
 
-export default function IconCard({ icon, progress }: IconCardProps) {
+const variants: Variants = {
+  initial: { opacity: 0, scale: 0 },
+  animate: { opacity: 1, scale: 1 },
+  exit: { opacity: 0, scale: 0, transition: { duration: 0.2 } },
+};
+
+/*
+  Do not pass other props than className, icon and progress to the div element!
+*/
+
+export default function IconCard({ icon, progress, ...props }: IconCardProps) {
   return (
-    <div className="flex relative bg-opacity-90 overflow-clip justify-center py-2 text-white bg-black px-[30px] last:rounded-r-md first:rounded-l-md ">
+    <motion.div
+      layout
+      variants={variants}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      className={cn(
+        "flex relative mx-0.5 bg-opacity-90 overflow-clip justify-center py-2 text-white bg-black px-[30px]",
+        props.className
+      )}
+    >
       <span className="relative z-50">{icon}</span>
       <div
         style={{ height: `${progress || 0}%` }}
-        className="absolute bottom-0 left-0 right-0 z-10 w-full transition-all duration-150 bg-blue-600"
+        className={cn(
+          "absolute bottom-0 left-0 right-0 z-10 w-full transition-all duration-150 bg-blue-600"
+        )}
       />
-    </div>
+    </motion.div>
   );
 }
