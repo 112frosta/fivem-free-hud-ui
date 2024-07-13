c1fadb8d62ee0c77e3cd54281ecefd22aa5d0a81
112frosta <112frosta@proton.me>
2024-07-13 13:33:41 +0200

    Add DeathScreen component to App.tsx

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,12 +1,14 @@
 import RootLayout from "@/layouts/RootLayout";
 import Hud from "@/elements/Hud";
 import Manifest from "@/elements/Manifest";
+import DeathScreen from "./elements/DeathScreen";
 
 export default function App() {
   return (
     <RootLayout>
       <Manifest />
       <Hud />
+      <DeathScreen />
     </RootLayout>
   );
 }
