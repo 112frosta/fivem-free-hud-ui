970f052c8b43dc17a192ab06da5aba2b380af9b9
112frosta <112frosta@proton.me>
2024-07-13 20:36:36 +0200

    Refactor CarHud component and add SpeedoMeter component with framer-motion animation

--- ui/src/elements/CarHud.tsx
+++ ui/src/elements/CarHud.tsx
@@ -1,67 +1,34 @@
-import { cn } from "@/utils/cn";
+import { motion, AnimatePresence, Variants } from "framer-motion";
+
+import SpeedoMeter from "@/components/SpeedoMeter";
 import { useState } from "react";
-import { FaCompass } from "react-icons/fa6";
-interface Item {
-  name: string;
-  value: string;
-  Icon?: typeof FaCompass;
-}
+import { useKeyboard } from "@/hooks/useKeyboard";
 
-const items: Item[] = [
-  { name: "Heading", value: "NE", Icon: FaCompass },
-  { name: "Street", value: "Innocence Blvd" },
-];
-
-function Card({ value, Icon }: Item) {
-  return (
-    <div className="flex relative items-center gap-2 first-of-type:rounded-l-md last-of-type:rounded-r-md bg-opacity-90 overflow-clip justify-center py-2.5 text-white bg-black px-7">
-      <span className="relative z-10">
-        {Icon && <Icon className="size-4" />}
-      </span>
-      <span className="text-sm font-medium">{value}</span>
-    </div>
-  );
-}
-
-function SpeedoMeter() {
-  //TODO: Implement speed state and update the speed value with Lua
-
-  const [speed] = useState(87);
-  const speedStr = speed.toString().padStart(3, "0");
-  const digits = speedStr.split("");
-
-  return (
-    <div className="flex flex-col gap-2">
-      <div className="p-5 space-y-5 text-right">
-        <div className="font-medium text-white">Brak Trasy</div>
-        <h1>
-          {digits.map((digit, index) => (
-            <span
-              key={index}
-              className={cn(
-                "font-black text-8xl text-content-100",
-                index === 0 && digit === "0" ? "opacity-50" : "opacity-90"
-              )}
-            >
-              {digit}
-            </span>
-          ))}
-          <span className="font-medium text-content-100">km/h</span>
-        </h1>
-      </div>
-      <div className="flex items-center gap-0.5">
-        {items.map((item, index) => (
-          <Card key={index} {...item} />
-        ))}
-      </div>
-    </div>
-  );
-}
+const variants: Variants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+};
 
 export default function CarHud() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useKeyboard("x", () => {
+    setIsVisible((prev) => !prev);
+  });
+
   return (
-    <div className="absolute flex bottom-5 right-12">
-      <SpeedoMeter />
-    </div>
+    <AnimatePresence mode="wait">
+      {isVisible && (
+        <motion.div
+          variants={variants}
+          initial="hidden"
+          animate="visible"
+          exit="hidden"
+          className="absolute flex bottom-5 right-12"
+        >
+          <SpeedoMeter />
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 }
