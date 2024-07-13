1f2ae5519e9b25facc1ddc7da08973d7b75b5270
112frosta <112frosta@proton.me>
2024-07-13 13:35:19 +0200

    Add DeathScreen component

---
+++ ui/src/elements/DeathScreen.tsx
@@ -0,0 +1,24 @@
+import Kbd from "@/components/Kbd";
+import { useState } from "react";
+import { formatTime } from "@/utils/formatTime";
+
+export default function DeathScreen() {
+  const [time, setTime] = useState(300);
+
+  return (
+    <div className="flex items-end justify-center gradient-background">
+      <div className="gradient-background animate-pulse" />
+      <div className="pb-48 space-y-2 text-center">
+        <p className="text-sm font-extrabold tracking-tight text-white uppercase animate-pulse opacity-80">
+          WYKRWAWIASZ SIĘ
+        </p>
+        <h1 className="text-6xl font-extrabold text-white opacity-80">
+          {formatTime(time)}
+        </h1>
+        <p className="font-medium text-white opacity-90">
+          Naciśnij <Kbd>G</Kbd> aby wezwać pomoc
+        </p>
+      </div>
+    </div>
+  );
+}
